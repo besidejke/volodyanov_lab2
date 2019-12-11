362e5ddbc7b758cb8860eb56558b33b7a93b4bb8
Volodyanov Nikita <56234640+besidejke@users.noreply.github.com>
2019-12-11 14:34:21 +0300

    Add files via upload

---
+++ src/TestAuthor.java
@@ -0,0 +1,12 @@
+public class TestAuthor {
+    public TestAuthor() {
+    }
+
+    public static void main(String[] args) {
+        Author a1 = new Author("Nikita", "volodyanov@yandex.ru", 'M');
+        System.out.println(a1);
+        a1.setEmail("test123@yandex.ru");
+        System.out.println(a1);
+    }
+}
+
